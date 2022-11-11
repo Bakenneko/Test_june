d217bf8cb97fd17cffe680d31b4c103e963fd911
Andrew Kurylyk <andronniy7@gmail.com>
2022-11-11 17:08:39 +0200

    lombok

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        new User();
+
+        User user2 = new User(2, "Jack", false, 55.5);
+        System.out.println(user2);
+
+//        user2.setName("Nick");
+//        System.out.println(user2.getName());
+//        user2.setName("Mickael");
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,17 @@
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+  private int id;
+  private String name;
+  private boolean status;
+  private double weight;
+
+  public String greeting(String msg) {
+      return msg + " my name is - " + this.name;
+  }
+}
